0296bbb6817be0f2756fdc41ccff3d048f8fc521
neelendra <npsy1811@gmail.com>
2025-02-03 23:15:07 +0530

    timestamp added

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-});
+},{timestamps : true} );
 
 // Hash password before saving
 userSchema.pre("save", async function (next) {
